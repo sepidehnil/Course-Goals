7588e72933731d9405610a0a07e9261a56ae8820
sepideh nilforoushan <3pd.nilforoushan@gmail.com>
2023-07-12 19:31:38 -0400

    add css module

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -1,9 +1,25 @@
-import "./Button.css";
+import React from "react";
+// import { styled } from "styled-components";
+import styles from "./Button.module.css";
+
+//styled component
+// const Button = styled.button`
+//   padding: 8px 10px;
+//   background-color: rgb(252, 38, 138);
+//   border: none;
+//   border-radius: 5px;
+//   @media (min-width: 768px) {
+//     width: 80px;
+//   }
+// `;
+
+//css modules
 function Button() {
   return (
     <div>
-      <input type="submit" value="Add Goal" id="btn-submit"/>
+      <button className={styles.button}>Add Goal</button>
     </div>
   );
 }
+
 export default Button;
--- src/components/GoalsInput/GoalsInput.jsx
+++ src/components/GoalsInput/GoalsInput.jsx
@@ -4,21 +4,39 @@ import "./GoalsInput.css";
 
 function GoalsInput(props) {
   const [inputValue, setInput] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
   const inputHandler = (event) => {
+    if (event.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setInput(event.target.value);
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (inputValue.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(inputValue);
   };
-  
+
   return (
     <form className="form-container" onSubmit={formSubmitHandler}>
-      <label id="title">Course Goals</label>
-      <input type="text" id="goal-input" onChange={inputHandler} />
-      <Button />
+      <label id="title" style={{ color: !isValid ? "red" : "black" }}>
+        Course Goals
+      </label>
+      <input
+        type="text"
+        id="goal-input"
+        style={{
+          borderColor: !isValid ? "red" : "black",
+          backgroundColor: !isValid ? "rgb(251, 158, 158)" : "transparent",
+        }}
+        onChange={inputHandler}
+      />
+      <Button>Add Goal</Button>
     </form>
   );
 }
